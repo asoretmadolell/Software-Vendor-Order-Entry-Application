7094fe22c8cdcccc2970db82a232dd29e07d846c
asoretmadolell <soret_40@hotmail.com>
2016-07-20 02:25:33 +0200

    Implementing the low-level function pw_find() as a stub function to look up the user name and password.
    pw_find() is another function called by id_user(). It looks up a user's
    name and password and returns if the user is found or not. Stub
    functions will simply be first versions of low-level functions to
    simulate their final versions.

--- Software-Vendor-Order-Entry-Application/STUBS.c
+++ Software-Vendor-Order-Entry-Application/STUBS.c
@@ -14,3 +14,29 @@
 /* Here are inv_find(), pw_find(), order_num(), and logentry()               */
 /*                                                                           */
 /*****************************************************************************/
+
+#include "STDDEFS.h"
+
+/*****************************************************************************/
+/*                                                                           */
+/* pw_find()                                                                 */
+/*                                                                           */
+/* Looks up a user's name and password and returns YES if the user is        */
+/* found, NO if not. If the user is found, office ID is also checked.        */
+/* User data contain indication of whether to ring bell.                     */
+/*                                                                           */
+/*****************************************************************************/
+flag pw_find( office, user, password )
+char office[];                  /* description of user's office (returned) */
+char user[];                    /* name, initials, or abbreviation of user to find */
+char password[];                /* password to be looked up */
+{
+    IMPORT bflag bell_ok;
+
+    if( user[ 1 ] == 'S' )      /* OK to ring the bell? */
+    {
+        bell_ok = NO;           /* No. Silence the bell. */
+    }
+
+    return( user[ 0 ] == 'S' && password[ 0 ] == 'S' );
+}
